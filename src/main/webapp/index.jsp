45ba4b71ff0f14e6cfdddc5880107fe83e4bd29f
wangdongsong <wangdongsong1229@163.com>
2015-11-01 11:52:14 +0800

    添加文件

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wds
+  Date: 2015/10/2
+  Time: 14:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ src/test/java/com/wds/netty/Test.java
@@ -0,0 +1,16 @@
+package com.wds.netty;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by wds on 2015/10/7.
+ */
+public class Test {
+    private static final Logger LOGGER = LogManager.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        LOGGER.info(System.getProperty("line.separator"));
+
+    }
+}
